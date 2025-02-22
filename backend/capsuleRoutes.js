50743617dda94024e62b5771f3328bedfcb30e20
Selina Wang <pengyun.w@wustl.edu>
2025-02-22 14:13:34 -0600

    update capsule structure

--- backend/capsuleRoutes.js
+++ backend/capsuleRoutes.js
@@ -11,13 +11,13 @@ router.post('/create_capsule', async (req, res) => {
   const db = req.app.locals.db;
   const capsulesCollection = db.collection("capsules");
 
-  // Insert into MongoDB
   const result = await capsulesCollection.insertOne({
     title,
     description,
     unlockDate: new Date(unlockDate), // Convert to Date object
     members,
     createdAt: new Date(),
+    isSealed: false
   });
 
   return res.status(201).json({ message: 'Capsule created successfully!' });
@@ -31,7 +31,7 @@ router.get('/get_all_capsules', async (req, res) => {
   try {
     const capsules = await capsulesCollection.find({}).toArray();
     return res.status(200).json(capsules);
-    
+
   } catch (error) {
     console.error("Error retrieving capsules:", error);
     return res.status(500).json({ message: 'Failed to retrieve capsules.' });
