62aa452a31e17b0e7c332a52c86e8be03a137eda
Selina Wang <pengyun.w@wustl.edu>
2025-02-25 15:56:33 -0600

    adjust homepage: timer, layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 import CreateCapsule from './capsule/CreateCapsule.js';
+import Capsule from './capsule/Capsule.js'
+import EditCapsule from './capsule/EditCapsule.js'
 import Letter from "./Letter.js";
 import LetterList from './LetterList.js';
 
@@ -16,7 +18,11 @@ const App = () => {
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/home" element={<Home />} />
+
         <Route path="/create-capsule" element={<CreateCapsule />} />
+        <Route path="/capsule" element={<Capsule />} />
+        <Route path="/edit-capsule" element={<EditCapsule />} />
+
         <Route path="/letter" element={<Letter />} />
         <Route path="/letter-list" element={<LetterList />} />
       </Routes>
