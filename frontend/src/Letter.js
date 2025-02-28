40dbca9f487a0f8d8c3c84dbfa8bccb3d48590cc
Elaine Choy <elainejchoy@gmail.com>
2025-02-28 15:40:36 -0600

    letter writing

--- frontend/src/Letter.js
+++ frontend/src/Letter.js
@@ -3,6 +3,9 @@ import { TextField, Button, Select, MenuItem, Box, Typography, ToggleButtonGroup
 import FormatBoldIcon from '@mui/icons-material/FormatBold';
 import FormatItalicIcon from '@mui/icons-material/FormatItalic';
 import jsPDF from 'jspdf';
+import './LetterEditor.css';
+import AppHeader from './HomePageComponents/AppHeader';
+
 
 export default function LetterEditor() {
     const [text, setText] = useState('');
@@ -23,6 +26,8 @@ export default function LetterEditor() {
             }
             const doc = new jsPDF();
             const formattedText = text;
+            doc.setFillColor(background);
+            doc.rect(0, 0, 210, 297, 'F');
             doc.setFontSize(12);
             if (formats.includes('bold')) doc.setFont(undefined, 'bold');
             if (formats.includes('italic')) doc.setFont(undefined, 'italic');
@@ -70,7 +75,12 @@ export default function LetterEditor() {
     };
 
     return (
-        <Box sx={{ p: 3, maxWidth: '600px', mx: 'auto' }}>
+        <>
+         <AppHeader />
+      
+        <Box className="letter-editor-container">
+           
+        <Box  className="letter-editor-box" sx={{ p: 3, maxWidth: '800px', mx: 'auto' }}>
             <Typography variant="h4" gutterBottom>Write Your Letter</Typography>
 
             <TextField
@@ -105,10 +115,11 @@ export default function LetterEditor() {
                 >
                     <MenuItem value="#ffffff">White</MenuItem>
                     <MenuItem value="#f0f0f0">Light Grey</MenuItem>
-                    <MenuItem value="#ffeb3b">Yellow</MenuItem>
+                    <MenuItem value="#fff99c">Yellow</MenuItem>
                     <MenuItem value="#c8e6c9">Green</MenuItem>
                     <MenuItem value="#bbdefb">Blue</MenuItem>
                     <MenuItem value="#eabfff">Purple</MenuItem>
+                    <MenuItem value="#ffc3ec">Pink</MenuItem>
                 </Select>
             </Box>
 
@@ -121,13 +132,15 @@ export default function LetterEditor() {
 
             <Button
                 variant="contained"
-                color="primary"
+                //color="primary"
                 fullWidth
                 onClick={generatePDFAndSubmit}
-                sx={{ mt: 2 }}
+                sx={{ mt: 2,  backgroundColor: '#c95eff', '&:hover': { backgroundColor: '#d98eff'}}}
             >
                 Submit Letter to Database
             </Button>
         </Box>
+        </Box>
+        </>
     );
 }
